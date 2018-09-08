05f5919215e19cb6293d7e4bf9a647ef324a5378
Niraj <npatel3@ufl.edu>
2018-09-08 15:13:53 -0400

    completed main functionalities and some styling

--- listingController.js
+++ listingController.js
@@ -7,17 +7,26 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {
-    
+     $scope.addListing = function() {
+        let newListing = {
+          code:$scope.buildingCode,
+          name:$scope.buildingName,
+          coordinates: {
+            latitude:$scope.buildingLatitude,
+            longitude:$scope.buildingLongitude
+          },
+          address:$scope.buildingAddress
+        };  
+        console.log(newListing);
+        $scope.listings.push(newListing);
     };
-  
+    
     //remove the entry from the list 
     $scope.deleteListing = function(index) {
       $scope.listings.splice(index, 1); 
     };
 
     $scope.showDetails = function(index) {
-      console.log($scope.listings[index]);
       $scope.detailedInfo = $scope.listings[index];
     };
   }
